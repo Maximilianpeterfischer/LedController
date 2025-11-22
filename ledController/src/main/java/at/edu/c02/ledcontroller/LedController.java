3fbb5147f0041c72247907fad8cf79cf6ae22b12
yngling-03 <Valentin.stern2@gmail.com>
2025-11-22 13:25:58 +0100

    added clock functionality, added matching tests

--- ledController/src/main/java/at/edu/c02/ledcontroller/LedController.java
+++ ledController/src/main/java/at/edu/c02/ledcontroller/LedController.java
@@ -27,6 +27,8 @@ public interface LedController {
 
     void spinningLed(String color, int turns, long sleepMillis) throws IOException, InterruptedException;
 
+    void showTime() throws IOException;
+
     void showTime(int hours, int minutes, int seconds) throws IOException;
 
     static String mixColors(boolean hour, boolean minute, boolean second) {
